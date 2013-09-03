91242a03abd422b32d7e57e17896954a589432d9
jinpeng <mr.li.jinpeng@gmail.com>
2013-09-03 17:32:55 +0200

    ENH: best search refit

--- test_best_search_refit.py
+++ test_best_search_refit.py
@@ -6,26 +6,39 @@
 from sklearn import datasets
 from sklearn.svm import SVC
 from epac import Methods
-from epac import CV
-from epac.workflow.splitters import CVBestSearchRefit
-from epac.map_reduce.reducers import ClassificationReport
-
+from epac.workflow.splitters import CVBestSearchRefit2
 
 X, y = datasets.make_classification(n_samples=12,
-n_features=10,
-n_informative=2,
-random_state=1)
+                                    n_features=10,
+                                    n_informative=2,
+                                    random_state=1)
 n_folds_nested = 2
 C_values = [.1, 0.5, 1, 2, 5]
 kernels = ["linear", "rbf"]
+
 methods = Methods(*[SVC(C=C, kernel=kernel)
     for C in C_values for kernel in kernels])
-sv_node = CV(methods, reducer=ClassificationReport(keep=False))
-sv_node.run(X=X, y=y)
-sv_node.reduce()
+wf = CVBestSearchRefit2(methods, n_folds=n_folds_nested)
 
-wf = CVBestSearchRefit(methods, n_folds=n_folds_nested)
-wf.transform(X=X, y=y)
+#wf.run(X=X, y=y)
+#for leaf in wf.walk_leaves():
+#    print leaf.load_results()
+#wf.reduce()
+
+from epac.map_reduce.engine import LocalEngine
+local_engine = LocalEngine(tree_root=wf, num_processes=2)
+wf = local_engine.run(X=X, y=y)
+for leaf in wf.walk_leaves():
+    print leaf.load_results()
 wf.reduce()
-wf.run(X=X, y=y)
-wf.reduce()
+
+#from epac.map_reduce.engine import SomaWorkflowEngine
+#sfw_engine = SomaWorkflowEngine(
+#                        tree_root=wf,
+#                        num_processes=3,
+#                        remove_finished_wf=False,
+#                        remove_local_tree=False)
+#wf = sfw_engine.run(X=X, y=y)
+#for leaf in wf.walk_leaves():
+#    print leaf.load_results()
+#wf.reduce()
---
+++ test_best_search_refit2.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+import unittest
+
+import string
+import os.path
+import numpy as np
+from sklearn import datasets
+from sklearn.svm import SVC
+from sklearn.feature_selection import SelectKBest
+from sklearn.cross_validation import StratifiedKFold
+from sklearn import grid_search
+
+from epac import Pipe, Methods, CV, Perms
+from epac import ClassificationReport, PvalPerms
+from epac import StoreFs
+from epac import CVBestSearchRefit
+from epac.sklearn_plugins import Permutations
+from epac.configuration import conf
+
+X, y = datasets.make_classification(n_samples=20,
+                                    n_features=10,
+                                    n_informative=2)
+n_folds = 2
+n_folds_nested = 3
+k_values = [1, 2]
+C_values = [1, 2]
+pipelines = Methods(*[
+                    Pipe(SelectKBest(k=k),
+                    Methods(*[SVC(kernel="linear", C=C)
+                    for C in C_values]))
+                    for k in k_values])
+
+pipeline = CVBestSearchRefit(pipelines,
+                             n_folds=n_folds_nested)
+
+tree_mem = CV(pipeline, n_folds=n_folds,
+              reducer=ClassificationReport(keep=False))
+# Save Tree
+import tempfile
+store = StoreFs(dirpath=tempfile.mkdtemp(), clear=True)
+# tree_mem.save_tree(store=store)
+tree_mem.run(X=X, y=y)
